dfc10c721e9bb1bcfe0ab809bae4ed2046872cb5
Ramprakash <chitti852852@gmail.com>
2024-10-13 04:44:54 +0530

    Initail commit

---
+++ scripts/deploy.js
@@ -0,0 +1,39 @@
+const hre = require("hardhat");
+
+const tokens = (nToken) => {
+    return ethers.utils.parseUnits(nToken.toString(),"ether");
+
+};
+
+
+async function main(params) {
+    //Woox Token
+    const _initialSupply = tokens(500000000000);
+    const Woox = await hre.ethers.getContractFactory("Woox");
+    const woox = await Woox.deploy(_initialSupply);
+    
+    
+    await woox.deployed();
+    console.log(`Woox: ${woox.address}`);
+
+    //ICO Woox
+    const _tokenPrice = tokens(0.0001);
+    const ICOWoox = await hre.ethers.getContractFactory("ICOWoox");
+    const icoWoox = await ICOWoox.deploy(_tokenPrice);
+
+    await icoWoox.deployed();
+    console.log(`ICOWoox: ${icoWoox.address}`);
+
+
+    //Liquidity
+    const Liquidity = await hre.ethers.getContractFactory("ICOWoox");
+    const liquidity = await Liquidity.deploy();
+
+    await liquidity.deployed();
+    console.log(`ICOWoox: ${liquidity.address}`);
+}
+
+main().catch((error) =>{
+    console.error(error);
+    Process.exitCode = 1;
+});
